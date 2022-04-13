20e34747253eac465d701a634f4500ac7aa41a4c
Vineeth Bhat <vineeth.bhat@gmail.com>
2022-04-13 16:28:58 +0530

    committing after doing Q3 - b

--- Q3/Q3.sh
+++ Q3/Q3.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 #Reading the file - 
-read $file_name
+read
 
 #a
-stat --printf="%s\n" $file_name
+stat --printf="%s\n" $REPLY
+
+#b
+cat -n $REPLY | tail -n1 | awk '{print $1}'
+
+#cat with n flag prints the file with line numbers
+#I access the last line using tail -n1
+#and print the first word, ie, the line number using awk
+
+#c 
+
