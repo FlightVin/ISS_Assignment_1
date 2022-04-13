602d3818969dbbe2c9c4840ffb81450814f4ed95
Vineeth Bhat <vineeth.bhat@gmail.com>
2022-04-13 22:56:07 +0530

    Finished Q4

--- Q4/Q4.sh
+++ Q4/Q4.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+IFS=',' #to read things separated by commas (,)
+read -a arr
+
+for (( i=0; i<${#arr[*]}; i++ ))
+do
+    for (( j=0; j<${#arr[*]}-$i-1; j++))
+    do
+        if [[ ${arr[$j]} > ${arr[$j+1]} ]]
+        then
+            temp_var=${arr[$j+1]}
+            arr[$j+1]=${arr[$j]} 
+            arr[$j]=$temp_var
+        fi
+    done
+done
+
+echo ${arr[*]}
